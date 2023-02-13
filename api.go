0573dfbde9716a9c24ae445bd6453b58e7dcf41d
Nathan <nathanmbicho@gmail.com>
2023-02-13 17:43:29 +0300

    - create an account type struct to handle account details, - manage handleFunc and printing out JSON data - handle route using gorilla/mux and account methods

--- api.go
+++ api.go
@@ -1,6 +1,37 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+type apiFunc func(w http.ResponseWriter, r *http.Request) error
+
+type ApiError struct {
+	Error string
+}
+
+func makeHTTPHandlerFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			err := WriteJSON(w, http.StatusBadRequest, ApiError{
+				Error: err.Error(),
+			})
+			if err != nil {
+				return
+			}
+		}
+	}
+}
 
 type APIServer struct {
 	listenAddr string
@@ -10,12 +41,38 @@ func NewAPIServer(listenAddr string) *APIServer {
 	return &APIServer{listenAddr: listenAddr}
 }
 
+func (s *APIServer) Run() {
+	router := mux.NewRouter()
+
+	router.Handle("/account", makeHTTPHandlerFunc(s.handleAccount))
+	router.Handle("/account/{id}", makeHTTPHandlerFunc(s.handleGetAccount))
+
+	log.Println("API server running on port", s.listenAddr)
+
+	err := http.ListenAndServe(s.listenAddr, router)
+	if err != nil {
+		return
+	}
+}
+
 func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
-	return nil
+	switch r.Method {
+	case "GET":
+		return s.handleGetAccount(w, r)
+	case "POST":
+		return s.handleCreateAccount(w, r)
+	case "DELETE":
+		return s.handleAccount(w, r)
+	default:
+		return fmt.Errorf("method not allowed %s", r.Method)
+	}
 }
 
 func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
-	return nil
+	id := mux.Vars(r)["id"]
+	fmt.Println(id)
+
+	return WriteJSON(w, http.StatusOK, &Account{})
 }
 
 func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
