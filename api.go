7c25a37ab419ee811cb5dcfae8446ceba70d50d0
Nathan <nathanmbicho@gmail.com>
2023-02-15 09:12:37 +0300

    - add content type application/json

--- api.go
+++ api.go
@@ -9,8 +9,8 @@ import (
 )
 
 func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(status)
-	w.Header().Set("Content-Type", "application/json")
 	return json.NewEncoder(w).Encode(v)
 }
 
